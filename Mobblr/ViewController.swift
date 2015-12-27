924ad2696b8bcbe5cdd7faf4bacb71d04bf7e3e1
karzler <mmishra23@gmail.com>
2015-12-28 02:57:23 +0530

    First commit

---
+++ Mobblr/ViewController.swift
@@ -0,0 +1,126 @@
+//
+//  ViewController.swift
+//  Mobblr
+//
+//  Created by Mohit Mishra on 12/28/15.
+//  Copyright © 2015 Mohit Mishra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPageViewControllerDataSource {
+    
+    var pageViewController: UIPageViewController!
+    var pageTitles: NSArray!
+    var pageImages: NSArray!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        self.pageTitles = NSArray(objects: "Post 1", "Post 2", "Post 3")
+        self.pageImages = NSArray(objects: "image1", "image2", "image3")
+        
+        self.pageViewController = self.storyboard?.instantiateViewControllerWithIdentifier("PageViewController") as! UIPageViewController
+        self.pageViewController.dataSource = self
+        
+        var startVC = self.viewControllerAtIndex(0) as ContentViewController
+        var viewControllers = NSArray(object: startVC)
+        
+        self.pageViewController.setViewControllers(viewControllers as! [UIViewController], direction: .Forward, animated: true, completion: nil)
+        
+        self.pageViewController.view.frame = CGRectMake(0, 30, self.view.frame.width, self.view.frame.size.height - 60)
+        
+        self.addChildViewController(self.pageViewController)
+        self.view.addSubview(self.pageViewController.view)
+        self.pageViewController.didMoveToParentViewController(self)
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func restartAction(sender: AnyObject)
+    {
+        var startVC = self.viewControllerAtIndex(0) as ContentViewController
+        var viewControllers = NSArray(object: startVC)
+        
+        self.pageViewController.setViewControllers(viewControllers as! [UIViewController], direction: .Forward, animated: true, completion: nil)
+    }
+    
+    func viewControllerAtIndex(index: Int) -> ContentViewController
+    {
+        if ((self.pageTitles.count == 0) || (index >= self.pageTitles.count)) {
+            return ContentViewController()
+        }
+        
+        var vc: ContentViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ContentViewController") as! ContentViewController
+        
+        vc.imageFile = self.pageImages[index] as! String
+        vc.titleText = self.pageTitles[index] as! String
+        vc.pageIndex = index
+        
+        return vc
+        
+        
+    }
+    
+    
+    // MARK: - Page View Controller Data Source
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController?
+    {
+        
+        var vc = viewController as! ContentViewController
+        var index = vc.pageIndex as Int
+        
+        
+        if (index == 0 || index == NSNotFound)
+        {
+            return nil
+            
+        }
+        
+        index--
+        return self.viewControllerAtIndex(index)
+        
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        var vc = viewController as! ContentViewController
+        var index = vc.pageIndex as Int
+        
+        if (index == NSNotFound)
+        {
+            return nil
+        }
+        
+        index++
+        
+        if (index == self.pageTitles.count)
+        {
+            return nil
+        }
+        
+        return self.viewControllerAtIndex(index)
+        
+    }
+    
+    /*func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int
+    {
+    return self.pageTitles.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int
+    {
+    return 0
+    }*/
+    
+}
+
+
+
